196ea880e22809c62aee54022c31f3e55323da40
Red-Portal <msca8h@naver.com>
2024-02-29 00:16:34 +0000

    fix Jacobian adjustment, minor refactors

--- src/model/likelihood.jl
+++ src/model/likelihood.jl
@@ -1,13 +1,13 @@
 
 function doa_normalgamma_likelihood(
-    filter ::AbstractDelayFilter,
-    Y      ::AbstractMatrix{Complex{T}}, 
-    yᵀy    ::T,
-    ϕ      ::AbstractVector{T},
-    λ      ::AbstractVector{T},
-    α      ::Real,
-    β      ::Real,
-    Δx     ::AbstractVector,
+    filter::AbstractDelayFilter,
+    Y     ::AbstractMatrix{Complex{T}}, 
+    yᵀy   ::T,
+    ϕ     ::AbstractVector{T},
+    λ     ::AbstractVector{T},
+    α     ::Real,
+    β     ::Real,
+    Δx    ::AbstractVector,
     c, 
     fs
 ) where {T <: Real}
@@ -19,7 +19,10 @@ function doa_normalgamma_likelihood(
     # = -(N + ν₀)/2 log( γ₀ + y†(I - H(W⁻¹ + H†H)⁻¹H†)y )
     # = -(N + ν₀)/2 log( γ₀ + y†y - y†(H(W⁻¹ + H†H)⁻¹H†)y )
     #
-    # det((H†WH + I)⁻¹) = det(W⁻¹) / det(W⁻¹ + H†H)
+    # det(P⊥)^{-1/2} (α/2 + y† P⊥ y)^{-MN/2 + β}
+    #
+    # det(P⊥) = det(H†WH + I) = det(W) det(W⁻¹ + H†H)
+    #
 
     N = size(Y,1)
     M = size(Y,2)
@@ -41,8 +44,8 @@ function doa_normalgamma_likelihood(
             return -Inf
         end
 
-        Tullio.@tullio threads=false ℓdetW⁻¹ := -N*log(λ[i])
-        ℓdetfactor = ℓdetW⁻¹ - ℓdetW⁻¹pHᴴH
+        Tullio.@tullio threads=false ℓdetW := N*log(λ[i])
+        ℓdetP⊥ = ℓdetW + ℓdetW⁻¹pHᴴH
 
         Tullio.@tullio threads=false Hᴴy[n,k] := conj(H[n,m,k]) * Y[n,m]
         Tullio.@tullio threads=false Py[n,k]  := W⁻¹pHᴴH⁻¹[n,k,j] * Hᴴy[n,j]
@@ -54,7 +57,7 @@ function doa_normalgamma_likelihood(
             return -Inf
         end
 
-        -(N*M/2 + β)*log(α/2 + yᴴImP⊥y) + ℓdetfactor/2
+        -(N*M/2 + β)*log(α/2 + yᴴImP⊥y) - ℓdetP⊥/2
     end
 end
 
@@ -82,13 +85,12 @@ function ReversibleJump.logdensity(
     elseif any((ϕ .< -π/2) .|| (ϕ .> π/2))
         -Inf
     else
-        λ      = exp.(ℓλ)
         ℓp_ϕ   = k*logpdf(Uniform(-π/2, π/2), 0.0)
         ℓp_λ   = sum(Base.Fix1(logpdf, InverseGamma(alpha_lambda, beta_lambda)), λ)
         ℓjac_λ = sum(ℓλ)
-        ℓp_y   =  doa_normalgamma_likelihood(
+        ℓp_y   = doa_normalgamma_likelihood(
             delay_filter, y_fft, y_power, ϕ, λ, alpha, beta, Δx, c, fs   
         )
-        ℓp_y + ℓp_ϕ + ℓp_λ + ℓp_k
+        ℓp_y + ℓp_ϕ + ℓp_λ + ℓp_k - ℓjac_λ
     end
 end
