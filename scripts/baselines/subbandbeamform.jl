0a1404562a0d7780f815f864e082fe0005a90195
Kyurae Kim <kyrkim@seas.upenn.edu>
2024-08-30 22:30:17 -0700

    add subband beamforming approaches

---
+++ scripts/baselines/subbandbeamform.jl
@@ -0,0 +1,66 @@
+
+function subbandbartlett(
+    R      ::Array,
+    θ_range::AbstractVector,
+    f_range::AbstractVector,
+    conf   ::ArrayConfig
+)
+    mapreduce(hcat, enumerate(f_range)) do (j, f)
+        Af = steering_matrix(θ_range, f_range[j], conf)
+        Rf = view(R, :, :, j)
+        real.(sum(conj(Af).*(Rf*Af), dims=1)[1,:])
+    end
+end
+
+function subbandmvdr(
+    R        ::Array,
+    θ_range  ::AbstractVector,
+    f_range  ::AbstractVector,
+    conf     ::ArrayConfig;
+    dl_factor::Real = 1e-3/size(R,1)
+)
+    mapreduce(hcat, enumerate(f_range)) do (j, f)
+        Af = steering_matrix(θ_range, f_range[j], conf)
+        Rf = view(R, :, :, j)
+
+        # Diagonal loading.
+        # Factor recommendd by Featherstone et al. (1997)
+        Rf_dl = Rf + tr(Rf)*dl_factor*I
+
+        ARinvA = sum(conj(Af).*(Rf_dl\Af), dims=1)[1,:]
+        @. 1 / real(ARinvA)
+    end
+end
+
+function cfar(
+    power  ::AbstractVector,
+    n_guard::Int,
+    n_train::Int,
+    gain   ::Real
+)
+    N = length(power)
+    peak_indices, peak_powers = Peaks.findmaxima(power)
+    sum(zip(peak_indices, peak_powers)) do (peak_idx, peak_power)
+        avg    = 0.0
+        n_data = 1
+
+        # Left train cells
+        for j in (peak_idx - n_guard - n_train):(peak_idx - n_guard - 1)
+            if j ≥ 1 && j ≤ N
+                avg     = (n_data-1)/(n_data)*avg + power[j]/n_data
+                n_data += 1
+            end
+        end
+
+        # Right train cells
+        for j in (peak_idx + n_guard + 1):(peak_idx + n_guard + n_train)
+            if j ≥ 1 && j ≤ N
+                avg     = (n_data-1)/(n_data)*avg + power[j]/n_data
+                n_data += 1
+            end
+        end
+
+        thres = gain*avg 
+        peak_power ≥ thres
+    end
+end
