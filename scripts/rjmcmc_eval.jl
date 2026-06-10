b26e2aa1be599f0a340c8d3cb2a39c9b2eed631a
Kyurae Kim <kyrkim@seas.upenn.edu>
2026-06-10 21:23:30 +0900

    fix tv curve estimation

--- scripts/rjmcmc_eval.jl
+++ scripts/rjmcmc_eval.jl
@@ -19,7 +19,7 @@ using mcmcse
 
 include("common.jl")
 
-function run_rjmcmc(rng, cond, n_samples, rjmcmc)
+function run_rjmcmc(rng, cond, n_samples, rjmcmc; show_progress=false)
     initial_params = WidebandDoA.WidebandIsoIsoParam{Float64}[]
     initial_order  = 0
     _, stats = ReversibleJump.sample(
@@ -29,23 +29,20 @@ function run_rjmcmc(rng, cond, n_samples, rjmcmc)
         n_samples,
         initial_order,
         initial_params;
-        show_progress=false,
+        show_progress,
     )
     return stats
 end
 
-function estimate_true_posterior(rng, cond)
-    n_burn    = 2^16
-    n_samples = 2^16
-
+function estimate_true_posterior(rng, cond, n_burn, n_samples; show_progress=false)
     prop   = UniformNormalLocalProposal(0.0, 1.0)
     mcmc   = SliceSteppingOut([2.0, 2.0])
     jump   = IndepJumpProposal(prop)
     rjmcmc = ReversibleJump.NonReversibleJumpMCMC(jump, mcmc; jump_rate=0.9)
 
-    stats = run_rjmcmc(rng, cond, n_samples + n_burn, rjmcmc)
+    stats = run_rjmcmc(rng, cond, n_samples + n_burn, rjmcmc; show_progress)
     stats = stats[n_burn+1:end]
-    return modelposterior_naive(stats), var([stat.order for stat in stats])
+    return modelposterior_naive(stats)
 end
 
 function total_variation_distance(p, q)
@@ -53,54 +50,72 @@ function total_variation_distance(p, q)
     return sum(abs, p.p - q.p)/2
 end
 
-function run_simulation(n_targets, snr, rjmcmc, n_burn, n_samples, n_reps)
-    stats = @showprogress pmap(1:n_reps) do key
+function run_simulation(
+    n_targets,
+    snr,
+    rjmcmc,
+    n_samples,
+    n_burn_truepost,
+    n_samples_truepost,
+    n_chains,
+    n_reps
+)
+    map(1:n_reps) do key
         seed = (0x38bef07cf9cc549d, 0x49e2430080b3f797)
         rng  = Philox4x(UInt64, seed, 8)
         set_counter!(rng, key)
 
-	N       = 128
+        N       = 128
         n_dft   = 1024
         fs      = 3000.0
-	model   = construct_default_model(N, fs)
+        model   = construct_default_model(N, fs)
         f_begin = 10
         f_end   = 1000
 
-        ϕ     = rand(rng, Uniform(-π/2, π/2), n_targets)
-	c, Δx = model.likelihood.c, model.likelihood.Δx
-	y, _  = simulate_signal(rng, N, n_dft, ϕ, snr, f_begin, f_end, fs, 1.0, Δx, c)
-	cond  = WidebandConditioned(model, y)
+        ϕ = if n_targets == 2
+            [0.5, -0.5]
+        elseif n_targets == 8
+            [0.8, 0.6, 0.4, 0.2, -0.2, -0.4, -0.6, -0.8]
+        else
+            error("not supported")
+        end
+        c, Δx = model.likelihood.c, model.likelihood.Δx
 
-        k_post_true, k_var_true = estimate_true_posterior(rng, cond)
+        y, _  = simulate_signal(rng, N, n_dft, ϕ, snr, f_begin, f_end, fs, 1.0, Δx, c)
+        cond  = WidebandConditioned(model, y)
 
-        stats = run_rjmcmc(rng, cond, n_samples + n_burn, rjmcmc)
+        k_post_true = estimate_true_posterior(rng, cond, n_burn_truepost, n_samples_truepost)
 
-        # Compute convergence in TV distance
-        sup      = k_post_true.support
-        hist     = Hist(0:maximum(sup) + 1)
-        tv_curve = map(stats) do stat
-            fit!(hist, stat.order)
-            _, counts = value(hist)
-            prob        = counts/sum(counts)
-            k_post      = DiscreteNonParametric(sup, prob)
+        k_chains = @showprogress pmap(1:n_chains) do key
+            seed = (0x38bef07cf9cc549d, 0x49e2430080b3f797)
+            rng  = Philox4x(UInt64, seed, 8)
+            set_counter!(rng, key)
+            stats = run_rjmcmc(rng, cond, n_samples, rjmcmc)
+            [stat.order for stat in stats]
+        end
+        
+        support  = k_post_true.support
+        tv_curve = map(1:n_samples) do t
+            hist = StatsBase.fit(
+                StatsBase.Histogram,
+                [k_chain[t] for k_chain in k_chains],
+                0:maximum(support) + 1
+        )
+            prob   = hist.weights / sum(hist.weights)
+            k_post = DiscreteNonParametric(support, prob)
             total_variation_distance(k_post, k_post_true)
         end
 
-        # Integrated autocorrelation time
-        k_chain = [stat.order for stat in stats[n_burn+1:end]]
-        iact    = if k_var_true < eps(Float32)
-            0
-        else
-            mcmcse.mcvar(k_chain, r=2)/k_var_true
-        end
-        (tv_curve = tv_curve, iact = iact, k_chain = k_chain)
+        (tv_curve = tv_curve,)
     end
 end
 
 function main()
-    n_burn    = 2^10
-    n_samples = 2^17
-    n_reps    = 32
+    n_burn_truepost    = 2^12
+    n_samples_truepost = 2^12
+    n_chains           = 64
+    n_samples          = 2^12
+    n_reps             = 4
 
     prop = UniformNormalLocalProposal(0.0, 2.0)
     mcmc = SliceSteppingOut([2.0, 2.0])
@@ -134,16 +149,24 @@ function main()
         end
 
         for snr in [-8, -4, 0, 4], n_targets in [2, 8]
-            @info("simualting", rjmcmc=rjmcmc_name, mcmc=mcmc_name, snr, n_targets)
+            @info("simulating", rjmcmc=rjmcmc_name, mcmc=mcmc_name, snr, n_targets)
 
-            stats = run_simulation(n_targets, snr, rjmcmc, n_burn, n_samples, n_reps)
+            stats = run_simulation(
+                n_targets,
+                snr,
+                rjmcmc,
+                n_samples,
+                n_burn_truepost,
+                n_samples_truepost,
+                n_chains,
+                n_reps
+            )
             df = DataFrame(
                 rjmcmc    = rjmcmc_name,
                 mcmc      = mcmc_name,
                 snr       = snr,
                 n_targets = n_targets,
                 tv_curve  = [stat.tv_curve for stat in stats],
-                iact      = [stat.iact     for stat in stats],
             )
             GC.gc()
             dfs = vcat(dfs, df)
